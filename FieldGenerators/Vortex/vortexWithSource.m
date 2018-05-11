65bde35f5987e00938abc58610d44bd571f4ccc1
RyanLoringCooper <ryanloringcooper@gmail.com>
2018-05-11 15:37:58 -0700

    added FieldGenerators

---
+++ FieldGenerators/Vortex/vortexWithSource.m
@@ -0,0 +1,8 @@
+function resp = vortexWithSource(x, y, varargin)
+    [u, v, un, vn, V] = PotentialFlow2DSim([2, 0, 0, 100, 0; 4, 0, 0, -100, 0], x, y);
+    if nargin == 3 && varargin{1}
+        resp = [un, vn];
+    else
+        resp = [u, v];
+    end    
+end
