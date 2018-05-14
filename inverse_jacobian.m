5e11059833e36ae10313c6b2fa51ce38b4d57470
RyanLoringCooper <ryanloringcooper@gmail.com>
2018-05-14 16:15:35 -0700

    documented things

--- inverse_jacobian.m
+++ inverse_jacobian.m
@@ -1,11 +1,11 @@
-function [ inv_Jac ] = inverse_jacobian( v )
+function Vr = inverse_jacobian( ClusterStateSpaceVector )
 %INVERSE_JACOBIAN Returns the inverse jacobian
 %   This is for a three bot p,q,beta cluster, in aero frame
 
-theta_c = v(1);
-p = v(2);
-q = v(3);
-beta1 = v(4);
+theta_c = ClusterStateSpaceVector(18);
+p = ClusterStateSpaceVector(19);
+q = ClusterStateSpaceVector(20);
+beta1 = ClusterStateSpaceVector(21);
 
 inv_Jac = [ 1, 0, -(sin(theta_c)*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2))/3, (cos(theta_c)*(2*p + 2*q*cos(beta1)))/(6*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2)), (cos(theta_c)*(2*q + 2*p*cos(beta1)))/(6*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2)), -(p*q*sin(beta1)*cos(theta_c))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2)), 0, 0, 0;
  0, 1,  (cos(theta_c)*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2))/3, (sin(theta_c)*(2*p + 2*q*cos(beta1)))/(6*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2)), (sin(theta_c)*(2*q + 2*p*cos(beta1)))/(6*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2)), -(p*q*sin(beta1)*sin(theta_c))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2)), 0, 0, 0;
@@ -16,7 +16,16 @@ inv_Jac = [ 1, 0, -(sin(theta_c)*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2))/3, (cos(t
  1, 0, (sin(theta_c)*(- p^2 + cos(beta1)*p*q + 2*q^2))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2)) - q*cos(theta_c)*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2),                                                                                       (cos(theta_c)*(4*p*q^2 + p^3 + q^3*cos(beta1) + 3*p^2*q*cos(beta1) - p*q^2*cos(beta1)^2))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(3/2)) + (p*q^2*sin(theta_c)*(q + p*cos(beta1))*(cos(beta1)^2 - 1))/((p^2 + 2*cos(beta1)*p*q + q^2)^2*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2)), - sin(theta_c)*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2) - (cos(theta_c)*(5*p^2*q + 2*q^3 + 2*p^3*cos(beta1) + 6*p*q^2*cos(beta1) + p^2*q*cos(beta1)^2))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(3/2)) - (p^2*q*sin(theta_c)*(q + p*cos(beta1))*(cos(beta1)^2 - 1))/((p^2 + 2*cos(beta1)*p*q + q^2)^2*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2)),   (p*q*sin(beta1)*cos(theta_c)*(2*p^2 + cos(beta1)*p*q - q^2))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(3/2)) - (p^2*q*sin(beta1)*sin(theta_c)*(p^2*cos(beta1) + p*q*cos(beta1)^2 + p*q + q^2*cos(beta1)))/((p^2 + 2*cos(beta1)*p*q + q^2)^2*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2)), 0, 0, 0;
  0, 1, - q*sin(theta_c)*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2) - (cos(theta_c)*(- p^2 + cos(beta1)*p*q + 2*q^2))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(1/2)), (sin(theta_c)*(4*p*q^2 + p^3 + q^3*cos(beta1) + 3*p^2*q*cos(beta1) - p*q^2*cos(beta1)^2))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(3/2)) - (p*q^2*cos(theta_c)*(q + p*cos(beta1))*(cos(beta1)^2 - 1))/((p^2 + 2*cos(beta1)*p*q + q^2)^2*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2)),   cos(theta_c)*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2) - (sin(theta_c)*(5*p^2*q + 2*q^3 + 2*p^3*cos(beta1) + 6*p*q^2*cos(beta1) + p^2*q*cos(beta1)^2))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(3/2)) + (p^2*q*cos(theta_c)*(q + p*cos(beta1))*(cos(beta1)^2 - 1))/((p^2 + 2*cos(beta1)*p*q + q^2)^2*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2)),   (p*q*sin(beta1)*sin(theta_c)*(2*p^2 + cos(beta1)*p*q - q^2))/(3*(p^2 + 2*cos(beta1)*p*q + q^2)^(3/2)) + (p^2*q*sin(beta1)*cos(theta_c)*(p^2*cos(beta1) + p*q*cos(beta1)^2 + p*q + q^2*cos(beta1)))/((p^2 + 2*cos(beta1)*p*q + q^2)^2*(-(p^2*(cos(beta1)^2 - 1))/(p^2 + 2*cos(beta1)*p*q + q^2))^(1/2)), 0, 0, 0;
  0, 0, 1, 0, 0, 0, 0, 0, 1];
-
-
+%Vc = [xc_dot; yc_dot; theta_c_dot; p_dot; q_dot; beta_dot; phi1_dot; phi2_dot; phi3_dot];
+Vc = [ ClusterStateSpaceVector(25); % xc_dot
+       ClusterStateSpaceVector(26); % yc_dot
+       ClusterStateSpaceVector(3);  % thetac_dot
+       ClusterStateSpaceVector(4);  % p_dot
+       ClusterStateSpaceVector(5);  % beta_dot
+       ClusterStateSpaceVector(6);  % q_dot
+       phi1_dot; %TODO              % phi1_dot
+       phi2_dot; %TODO              % phi2_dot
+       phi3_dot]; %TODO             % phi3_dot
+Vr = inv_Jac*Vc;
 end
 
