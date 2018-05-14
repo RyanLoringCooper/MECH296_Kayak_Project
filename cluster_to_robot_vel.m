8636b2f52892f8c0d7e383a0af3c3ee954e35685
RyanLoringCooper <ryanloringcooper@gmail.com>
2018-05-14 15:27:52 -0700

    can specifiy cluster holonomicy from simulink now

--- cluster_to_robot_vel.m
+++
@@ -1,5 +0,0 @@
-function robot_vel_cmd = cluster_to_robot_vel( u )
-	% jacobian is 9x9 matrix computed from theta, p, q, and beta
-	J = inverse_jacobian( u(10:13) );
-	robot_vel_cmd = J*u(1:9);
-end
