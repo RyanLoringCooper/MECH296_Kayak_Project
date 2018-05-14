2d20b7b23766b9b2f9219bc812595820a7f6adc4
RyanLoringCooper <ryanloringcooper@gmail.com>
2018-05-14 10:53:27 -0700

    added file

---
+++ heading_error_conditioner.m
@@ -0,0 +1,27 @@
+function Output = heading_error_conditioner(u)
+%This function produces heading angle error conditioning so there's no jump
+%at angular discontinuities
+%arguments:     u = [angular_error]
+%output:     output = [conditioned_angular_error]
+
+%Initialize variables
+threshold=pi;
+
+e = u(1);
+e=mod(e,2*pi);
+
+if abs(e) > threshold
+    if e<0
+        e=e+2*pi;
+        %e=min(e,pi+e);
+
+    else
+        e=e-2*pi;
+        %e=min(e,pi-e);
+    end
+    
+end
+
+e=min(e,pi-e);
+
+Output = e;
