80bee108649664556c6f2bc624a72b6523780e63
RyanLoringCooper <ryanloringcooper@gmail.com>
2018-05-17 12:07:02 -0700

    who knows why it doesn't work

--- AlgorithmSolvers/AlgWrapper.m
+++ AlgorithmSolvers/AlgWrapper.m
@@ -31,56 +31,63 @@ function ClusterStateSpaceVector = AlgWrapper(AlgToCall, RobotStateSpaceVector, 
     clusterTheta = clusterVars(3);
 
     %Rotation matrix from global frame to cluster frame.
-    Rcg = [cos(clusterTheta), sin(clusterTheta), 0; 
-           -sin(clusterTheta), cos(clusterTheta), 0;
-           0, 0, 1];
-
-    des_theta = atan2(g1_unit(2), g1_unit(1));
-    thetac_dot = (des_theta - clusterTheta);
-
-    p_dot = 0; %Can be used to change the size of the cluster
-    q_dot = 0;
-    beta_dot = 0;
-    if isClusterHolonomic
+%      Rcg = [cos(clusterTheta), sin(clusterTheta), 0; 
+%             -sin(clusterTheta), cos(clusterTheta), 0;
+%             0, 0, 1];
+ 
+     des_theta = atan2(g1_unit(2), g1_unit(1));
+     disp(size(des_theta));
+     disp(size(clusterTheta));
+     dtheta = double(des_theta);
+     ctheta = double(clusterTheta);
+     thetacdot = dtheta*1 + (-1*ctheta);
+%      thetac_dot = double(des_theta)-double(clusterTheta);
+     p_dot = 0; %Can be used to change the size of the cluster
+     q_dot = 0;
+     beta_dot = 0;
+     if isClusterHolonomic
         yc_dot = sin(des_theta);
         xc_dot = cos(des_theta);
-    else
+     else
         yc_dot = 0;
         xc_dot = 1; % makes the command non holonomic
-    end
+     end
 
+     Rgc = [cos(clusterTheta), -sin(clusterTheta);
+            sin(clusterTheta), cos(clusterTheta)];
+       
     %Transformation into global frame
-    vg_dot = Rgc*([xc_dot yc_dot]');
-    xg_dot = vg_dot(1);
-    yg_dot = vg_dot(2);
+     vg_dot = Rgc*([xc_dot yc_dot]');
+     xg_dot = vg_dot(1);
+     yg_dot = vg_dot(2);
     
-    ClusterStateSpaceVector = [
-        xg_dot;                                 %1
-        yg_dot;                                 %2
-        theatc_dot;                             %3
-        p_dot;                                  %4
-        q_dot;                                  %5
-        beta_dot;                               %6
-        r1_pos(1);                  %R1x        %7
-        r1_pos(2);                  %R1y        %8
-        r1_pos(3);                  %phi1       %9
-        r2_pos(1);                  %R2x        %10
-        r2_pos(2);                  %R2y        %11
-        r2_pos(3);                  %phi2       %12
-        r3_pos(1);                  %R3x        %13
-        r3_pos(2);                  %R3y        %14
-        r3_pos(3);                  %phi3       %15
-        clusterVars(1);             %xc         %16
-        clusterVars(2);             %yc         %17
-        clusterVars(3);             %theta_c    %18
-        clusterVars(4);             %p          %19
-        clusterVars(5);             %q          %20
-        clusterVars(6);             %beta       %21
-        RobotStateSpaceVector(12);  %phi1_dot   %22
-        RobotStateSpaceVector(15);  %phi2_dot   %23
-        RobotStateSpaceVector(18);  %phi3_dot   %24
-        xc_dot;                                 %25
-        yc_dot;                                 %26
-        ];
-    ClusterStateSpaceVector = ClusterStateSpaceVector';
+%     ClusterStateSpaceVector = [
+%         xg_dot;                                 %1
+%         yg_dot;                                 %2
+%         theatc_dot;                             %3
+%         p_dot;                                  %4
+%         q_dot;                                  %5
+%         beta_dot;                               %6
+%         r1_pos(1);                  %R1x        %7
+%         r1_pos(2);                  %R1y        %8
+%         r1_pos(3);                  %phi1       %9
+%         r2_pos(1);                  %R2x        %10
+%         r2_pos(2);                  %R2y        %11
+%         r2_pos(3);                  %phi2       %12
+%         r3_pos(1);                  %R3x        %13
+%         r3_pos(2);                  %R3y        %14
+%         r3_pos(3);                  %phi3       %15
+%         clusterVars(1);             %xc         %16
+%         clusterVars(2);             %yc         %17
+%         clusterVars(3);             %theta_c    %18
+%         clusterVars(4);             %p          %19
+%         clusterVars(5);             %q          %20
+%         clusterVars(6);             %beta       %21
+%         RobotStateSpaceVector(12);  %phi1_dot   %22
+%         RobotStateSpaceVector(15);  %phi2_dot   %23
+%         RobotStateSpaceVector(18);  %phi3_dot   %24
+%         xc_dot;                                 %25
+%         yc_dot;                                 %26
+%         ];
+    ClusterStateSpaceVector = zeros(26, 1) %ClusterStateSpaceVector';
 end
