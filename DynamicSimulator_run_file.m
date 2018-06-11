08d82d1020682c31ed58ae3986723f2ca58544ed
garren lee lewis hendricks <ghendric@dcengr.ad.engr>
2018-06-11 16:26:29 -0700

    added more plots

--- DynamicSimulator_run_file.m
+++ DynamicSimulator_run_file.m
@@ -1,7 +1,7 @@
 clear; clc;
 sim('DynamicSimulator');
 path_resolution = 3;
-fieldGenerator = 'doubleSource'; %this must match the corresponding fieldGenerator box in DynamicSimulator.slx
+fieldGenerator = 'source'; %this must match the corresponding fieldGenerator box in DynamicSimulator.slx
 algSolver = 'contourFollower';
 width = 100;
 vector_field_resolution = 1;
@@ -9,5 +9,5 @@ vector_field_resolution = 1;
 %plot_path(ClusterStateSpaceTimeSeries, path_resolution, width, fieldGenerator,  algSolver, vector_field_resolution);
 %plot_animation(ClusterStateSpaceTimeSeries, path_resolution, width, fieldGenerator,  algSolver, vector_field_resolution);
 %plot_contour_frame(ClusterStateSpaceTimeSeries, path_resolution, width, fieldGenerator,  algSolver, vector_field_resolution);
-%plot_contour_frame_3D(ClusterStateSpaceTimeSeries, path_resolution, width, fieldGenerator,  algSolver, vector_field_resolution);
-plot_contour_animation(ClusterStateSpaceTimeSeries, path_resolution, width, fieldGenerator, algSolver, vector_field_resolution);
+plot_contour_frame_3D(ClusterStateSpaceTimeSeries, path_resolution, width, fieldGenerator,  algSolver, vector_field_resolution);
+%plot_contour_animation(ClusterStateSpaceTimeSeries, path_resolution, width, fieldGenerator, algSolver, vector_field_resolution);
